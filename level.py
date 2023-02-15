1dd2b47c28d9790da56a0187e6e08489054a7e35
John <juaorok@hotmail.com>
2023-02-15 20:45:50 -0300

    Update level.py

--- level.py
+++ level.py
@@ -181,7 +181,7 @@ class CameraGroup(pygame.sprite.Group):
         self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
         for layer in LAYERS.values():
-            for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            for sprite in sorted(self.sprites(), key=lambda sprite: sprite.z * 10000 + sprite.rect.centery):
                 if sprite.z == layer:
                     offset_rect = sprite.rect.copy()
                     offset_rect.center -= self.offset
